74b7008dc77594d010a4e3c79bbece765f76448e
Wxcvbn,;:=1 <2018mymess@gmail.com>
2018-02-11 22:09:27 +0100

    rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET'],
-    :region                => ENV['S3_REGION']
+    #:region                => ENV['S3_REGION']
 
   }
 
@@ -30,6 +30,6 @@ CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
   config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.fog_public = false
+  config.cache_dir     = "#{Rails.root}/tmp/uploads"
 
 end

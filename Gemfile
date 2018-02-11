17abe54871a603116a7dbdb74bb6347a5db98ce7
Wxcvbn,;:=1 <2018mymess@gmail.com>
2018-02-11 16:44:19 +0100

    intallation S3

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '5.1.4'
 gem 'redis'
 gem 'devise'
 gem 'carrierwave'
+gem 'fog'
 
 
 
--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -7,13 +7,13 @@ class VideoUploader < CarrierWave::Uploader::Base
 
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-   #storage :fog
+  #storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    #"uploads/<unique_guid>/foo.png"
+
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.user_id}"
   end
 
